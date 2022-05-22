adca2e9287b398fab42e560c69d1a1e0d7842366
Luis Dudamel <ladusa@gmail.com>
2022-05-22 19:41:27 +0200

    Failed thunk test

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,7 +1,14 @@
 import { rest } from "msw";
 
 export const handlers = [
-  rest.post("users/login", (req, res, ctx) => {
-    return res(ctx.status(200));
+  rest.post("http://localhost:4001/users/login/", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data: {
+          token: "token",
+        },
+      })
+    );
   }),
 ];
--- src/redux/thunks/usersThunks.test.js
+++ src/redux/thunks/usersThunks.test.js
@@ -1,10 +1,17 @@
+import { loginThunk } from "./usersThunks";
+
 describe("Given a userThunks thunk", () => {
   describe("When its invoked with username 'luis1' and password '1234'", () => {
-    test("Then it should call the setItem method, with the token received", () => {
-      const username = {
+    test("Then it should call the setItem method, with the token received", async () => {
+      const req = {
         username: "luis1",
         password: "1234",
       };
+
+      const res = await loginThunk(req);
+      const expectedTokenSet = await sessionStorage.getItem("token");
+
+      expect(expectedTokenSet).not.toBeNull();
     });
   });
 });
